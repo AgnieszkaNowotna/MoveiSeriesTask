77a34322472d9e01321db0e70b58cf2b67df4187
Agnieszka Nowotna <a.ilkow7@gmail.com>
2023-02-01 14:56:24 +0100

    Add new methods

--- MovieLibrary.py
+++ MovieLibrary.py
@@ -5,6 +5,11 @@ class Movie():
         self.genre = genre
         self.views = views
 
+    def __str__(self):
+        return f'"{self.title}" ({self.year})'
+
+    def play(self, add_views =1):
+        views += add_views
 
 class Series(Movie):
     def __init__(self, episode, season, *args, **kwargs):
@@ -12,5 +17,8 @@ class Series(Movie):
         self.episode = episode
         self.season = season
 
+    def __str__(self):
+        return f'"{self.title}" S{self.season:02d}E{self.episode:02d}'
 
+movie_series_list = []
 
